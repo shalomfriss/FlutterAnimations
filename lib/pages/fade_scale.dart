702e3edb6fb78c5d28e7a2a54e5f7bc9e7475112
Shalom Friss <shalomfriss@gmail.com>
2021-11-01 22:09:35 -0500

    :added fade animation

--- lib/pages/fade_scale.dart
+++ lib/pages/fade_scale.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
 class FadeScaleTransitionDemo extends StatefulWidget {
@@ -11,6 +12,44 @@ class FadeScaleTransitionDemo extends StatefulWidget {
 class _FadeScaleTransitionDemoState extends State<FadeScaleTransitionDemo> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Fade')),
+      body: ElevatedButton(
+        onPressed: () {
+          //This function is in the animation library and creates a fade scale animation
+          //https://pub.dev/documentation/animations/latest/animations/showModal.html
+          showModal<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return _ExampleAlertDialog();
+            },
+          );
+        },
+        child: const Text('SHOW MODAL'),
+      ),
+    );
+  }
+}
+
+class _ExampleAlertDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: const Text('Alert Dialog'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('CANCEL'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('DISCARD'),
+        ),
+      ],
+    );
   }
 }
--- lib/pages/fade_through.dart
+++ lib/pages/fade_through.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
 class FadeThroughTransitionDemo extends StatefulWidget {
@@ -9,8 +10,120 @@ class FadeThroughTransitionDemo extends StatefulWidget {
 }
 
 class _FadeThroughTransitionDemoState extends State<FadeThroughTransitionDemo> {
+  int pageIndex = 0;
+  List<Widget> pageList = <Widget>[
+    _FirstPage(),
+    _SecondPage(),
+    _ThirdPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Fade through')),
+      body: PageTransitionSwitcher(
+        transitionBuilder: (
+          Widget child,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,
+        ) {
+          return FadeThroughTransition(
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+            child: child,
+          );
+        },
+        child: pageList[pageIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: pageIndex,
+        onTap: (int newValue) {
+          setState(() {
+            pageIndex = newValue;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_library),
+            label: 'Albums',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo),
+            label: 'Photos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(width: 300, height: 100, color: Colors.yellow)
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(width: 300, height: 100, color: Colors.green)
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(width: 300, height: 100, color: Colors.blue)
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Expanded(child: Container(width: 100, height: 100, color: Colors.red))
+      ],
+    );
+  }
+}
+
+class _ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text('List item ${index + 1}'),
+          subtitle: const Text('Secondary text'),
+        );
+      },
+      itemCount: 10,
+    );
   }
 }
